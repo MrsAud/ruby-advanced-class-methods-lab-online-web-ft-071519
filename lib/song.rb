7d80bb9834fa25eeef372fb0a6424883a01a22a5
Audrea Cook <audreacwork@gmail.com>
2019-07-23 21:08:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    @@all.find{|song| song.name == name}
+    self.all.find{|song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
@@ -42,7 +42,7 @@ class Song
   end
   
   def self.alphabetical
-    @@all.sort_by {|a, b| a <=> b}
+    self.all.sort_by {|a, b| a <=> b}
   end
 
 end
