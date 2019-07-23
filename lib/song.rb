23253d1f880377f7d397a77d28f09448ba52a1b2
Audrea Cook <audreacwork@gmail.com>
2019-07-23 21:04:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end
   
   def self.alphabetical
-    self.all.name.sort_by {|song_a, song_b| song_a <=> song_b}
+    self.name.sort_by {|song_a, song_b| song_a <=> song_b}
   end
 
 end
