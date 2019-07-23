a73c5bc885af1440778c591688cb1145e4b0d286
Audrea Cook <audreacwork@gmail.com>
2019-07-23 20:22:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,11 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
     @@all << song
     song
   end
