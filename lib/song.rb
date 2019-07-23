2a692fc52fd4a3797519e004546b0c0f6e493556
Audrea Cook <audreacwork@gmail.com>
2019-07-23 20:32:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,12 +34,11 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
-      if true
-        self
-      else
-        self.create_by_name(name)
-      end
+    if self.find_by_name(name)
+      self
+    else
+      self.create_by_name(name)
+    end
   end
 
 end
